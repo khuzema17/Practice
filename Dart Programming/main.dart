43c30b7e5eac3d149aa836f5b895c2981ade4f71
Khuzema Ahmed <khuzemaahmed26@outlook.com>
2021-05-14 19:26:28 +0500

    Initial commit

---
+++ Dart Programming/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  print(addition());
+}
+
+dynamic addition() {
+  var num1 = 5;
+  var num2 = 7;
+  var sum = num1 + num2;
+  return sum;
+}
---
+++ khuzema_start/android/app/src/main/kotlin/com/example/khuzema_start/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.khuzema_start
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
